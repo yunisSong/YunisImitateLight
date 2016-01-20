84f00b2ee4dfe7d20821873300d4d15ece09979c
Yunis <332963965@qq.com>
2016-01-20 19:13:48 +0800

    Initial Commit

---
+++ YunisImitateLight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YunisImitateLight
+//
+//  Created by Yunis on 16/1/20.
+//  Copyright © 2016年 Yunis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YunisImitateLight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YunisImitateLight
+//
+//  Created by Yunis on 16/1/20.
+//  Copyright © 2016年 Yunis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
